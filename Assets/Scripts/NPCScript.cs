8e4ca58af369d22f47a86ee31679fc3db1cf0fe0
HualiangZ <zhualiang88@gmail.com>
2024-07-16 11:29:29 +0100

    update npc script

--- Assets/Scripts/NPCScript.cs
+++ Assets/Scripts/NPCScript.cs
@@ -6,12 +6,23 @@ using UnityEngine;
 public class NPCScript : MonoBehaviour
 {
     public States NPCStates;
+    public List<Actions> actions = new List<Actions>();
+    private Queue<Actions> actionQ;
+    public Actions currentAction;
     // Start is called before the first frame update
     void Start()
     {
         NPCStates = new States();
+        Actions[] act = GetComponents<Actions>();
+        foreach (Actions a in act) 
+        {
+            actions.Add(a);
+        }
+
+
         NPCStates.AddState("Hunger", 100);
         NPCStates.AddState("Thirst", 100);
+
     }
 
     // Update is called once per frame
